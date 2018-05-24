3add8263811c30be3900ecc5ffc41f10be400adc
ecoulson <ivansonofcoul@gmail.com>
2018-05-23 23:51:45 -0700

    added FileLine class

---
+++ Line.js
@@ -0,0 +1,9 @@
+class Line {
+	constructor(functionExpression, interval, result) {
+		this.functionExpression = functionExpression;
+		this.interval = interval;
+		this.result = result;
+	}
+}
+
+module.exports = Line;
